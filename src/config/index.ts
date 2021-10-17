df023c708fdfa71be8f684eff17b853869b2a828
Eduardo Tioma <tioma.eduardo@gmail.com>
2021-10-17 12:27:41 -0300

    consertar BASE_URL

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,3 +1,4 @@
-export const BASE_URL = process.env.NEXT_PUBLIC_VERCEL_URL
-  ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
-  : 'http://localhost:3000';
+export const BASE_URL =
+  process.env.NEXT_PUBLIC_VERCEL_ENV === 'production'
+    ? 'https://pulmeo.vercel.app'
+    : 'http://localhost:3000';
