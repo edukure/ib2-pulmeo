afd903556513a942a45efe7caaa2b7d47b1650d2
Eduardo Tioma <tioma.eduardo@gmail.com>
2021-10-24 17:05:35 -0300

    refatorar instrucoes para componente proprio

---
+++ src/components/Exames/Oximetria/Instrucoes/index.tsx
@@ -0,0 +1,97 @@
+import React from 'react';
+import { Button, VStack, HStack, Tag, Spinner } from '@chakra-ui/react';
+import { useSteps } from 'chakra-ui-steps';
+import { FaBluetoothB } from 'react-icons/fa';
+
+import Passos, { listaPassos } from '../Passos';
+
+type InstrucoesProps = {
+  connectionStatus: 'connecting' | 'disconnected' | 'connected';
+  handleConectarBluetooth: () => {};
+  handleFim: () => void;
+};
+
+function Instrucoes({
+  connectionStatus,
+  handleConectarBluetooth,
+  handleFim,
+}: InstrucoesProps) {
+  const { nextStep, prevStep, activeStep } = useSteps({
+    initialStep: 0,
+  });
+
+  const handleAvancar = () => {
+    if (activeStep < listaPassos.length - 1) {
+      nextStep();
+    }
+
+    if (activeStep === listaPassos.length - 1) {
+      handleFim();
+      console.log('fim');
+    }
+  };
+
+  const handleVoltar = () => {
+    if (activeStep > 0) {
+      prevStep();
+    }
+  };
+
+  return (
+    <>
+      <Passos ativo={activeStep} />
+
+      <VStack w="full" h="full" pb={4} px={2} fontSize="1.5rem">
+        {listaPassos[activeStep].componente}
+
+        {connectionStatus === 'connecting' && (
+          <>
+            <Spinner size="lg" thickness="4px" color="blue.500" />
+            {/* <Button colorScheme="red" isFullWidth onClick={disconnect}>
+            Cancelar
+          </Button> */}
+          </>
+        )}
+
+        {connectionStatus === 'disconnected' && activeStep === 2 && (
+          <Button
+            isFullWidth
+            colorScheme="messenger"
+            onClick={handleConectarBluetooth}
+            rightIcon={<FaBluetoothB />}
+          >
+            Conectar
+          </Button>
+        )}
+
+        {connectionStatus === 'connected' && activeStep === 2 && (
+          <Tag px={4} py={2} colorScheme="messenger">
+            Oxímetro conectado!
+          </Tag>
+        )}
+
+        <HStack justifyContent="flex-end" w="full">
+          <Button
+            onClick={handleVoltar}
+            w="full"
+            colorScheme="blackAlpha"
+            display={activeStep === 0 ? 'none' : 'block'}
+          >
+            Voltar
+          </Button>
+
+          <Button
+            onClick={handleAvancar}
+            w="full"
+            colorScheme="teal"
+            // disabled={connectionStatus !== 'connected' && activeStep === 2}
+          >
+            {activeStep === listaPassos.length - 1 ? 'Iniciar' : 'Avançar'}
+          </Button>
+        </HStack>
+      </VStack>
+    </>
+  );
+}
+
+export default Instrucoes;
--- src/components/Exames/Oximetria/Screen/index.tsx
+++ src/components/Exames/Oximetria/Screen/index.tsx
@@ -1,16 +1,8 @@
-import {
-  Button,
-  VStack,
-  Heading,
-  HStack,
-  Tag,
-  Spinner,
-} from '@chakra-ui/react';
-import { useSteps } from 'chakra-ui-steps';
-import { FaBluetoothB } from 'react-icons/fa';
+import { VStack, Heading } from '@chakra-ui/react';
 
 import useBluetooth from '@hooks/useBluetooth';
-import Passos, { listaPassos } from '../Passos';
+import { useState } from 'react';
+import Instrucoes from '../Instrucoes';
 
 type OximetriaScreenProps = {
   id: string;
@@ -23,6 +15,8 @@ type OximetriaScreenProps = {
 
 // Descriptor fc3cca29-fc97-420c-9bd0-39485cdcdbbc
 
+type Etapa = 'instrucoes' | 'coleta' | 'fim';
+
 function OximetriaScreen({ id, nome }: OximetriaScreenProps) {
   const { start, stop, values, getDeviceInfo, connectionStatus, disconnect } =
     useBluetooth(
@@ -31,21 +25,7 @@ function OximetriaScreen({ id, nome }: OximetriaScreenProps) {
       'f89bcd15-58d3-4465-a240-e6420b3ede5e'
     );
 
-  const { nextStep, prevStep, activeStep } = useSteps({
-    initialStep: 0,
-  });
-
-  const handleAvancar = () => {
-    if (activeStep < listaPassos.length - 1) {
-      nextStep();
-    }
-  };
-
-  const handleVoltar = () => {
-    if (activeStep > 0) {
-      prevStep();
-    }
-  };
+  const [etapa, setEtapa] = useState<Etapa>('instrucoes');
 
   const handleConectarBluetooth = async () => {
     await getDeviceInfo();
@@ -64,57 +44,25 @@ function OximetriaScreen({ id, nome }: OximetriaScreenProps) {
         Oximetria
       </Heading>
 
-      <Passos ativo={activeStep} />
+      {etapa === 'instrucoes' && (
+        <Instrucoes
+          connectionStatus={connectionStatus}
+          handleConectarBluetooth={handleConectarBluetooth}
+          handleFim={() => {
+            setEtapa('coleta');
+          }}
+        />
+      )}
 
-      <VStack w="full" h="full" pb={4} px={2} fontSize="1.5rem">
-        {listaPassos[activeStep].componente}
-
-        {connectionStatus === 'connecting' && (
-          <>
-            <Spinner size="lg" thickness="4px" color="blue.500" />
-            {/* <Button colorScheme="red" isFullWidth onClick={disconnect}>
-              Cancelar
-            </Button> */}
-          </>
-        )}
-
-        {connectionStatus === 'disconnected' && activeStep === 2 && (
-          <Button
-            isFullWidth
-            colorScheme="messenger"
-            onClick={handleConectarBluetooth}
-            rightIcon={<FaBluetoothB />}
-          >
-            Conectar
-          </Button>
-        )}
-
-        {connectionStatus === 'connected' && activeStep === 2 && (
-          <Tag px={4} py={2} colorScheme="messenger">
-            Oxímetro conectado!
-          </Tag>
-        )}
-
-        <HStack justifyContent="flex-end" w="full">
-          <Button
-            onClick={handleVoltar}
-            w="full"
-            colorScheme="blackAlpha"
-            display={activeStep === 0 ? 'none' : 'block'}
-          >
-            Voltar
-          </Button>
-
-          <Button
-            onClick={handleAvancar}
-            w="full"
-            colorScheme="teal"
-            // disabled={connectionStatus !== 'connected' && activeStep === 2}
-          >
-            Avançar
-          </Button>
-        </HStack>
-      </VStack>
+      {etapa === 'coleta' && (
+        <Instrucoes
+          connectionStatus={connectionStatus}
+          handleConectarBluetooth={handleConectarBluetooth}
+          handleFim={() => {
+            setEtapa('coleta');
+          }}
+        />
+      )}
     </VStack>
   );
 }
